3464437c7170ff0c5d05d15f8bb222a9a6ccd7f6
Dmytro <pase4nikdv@gmail.com>
2023-03-14 18:48:00 +0100

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+import NotFound from './pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <header className="header">
+        <nav>
+          <NavLink className="nav-link" to="/" end>
+            {' '}
+            Home
+          </NavLink>
+          <NavLink className="nav-link" to="/movies">
+            {' '}
+            Movies{' '}
+          </NavLink>
+        </nav>
+      </header>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </div>
   );
 };
---
+++ src/components/pages/MoviesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MoviesPage = () => {
+  return <h1>Hello world on Movies PAGE</h1>;
+};
+
+export default MoviesPage;
