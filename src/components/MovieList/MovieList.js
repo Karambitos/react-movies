21362731aaa782d5f3af2f48fb3e668e34fbb723
Dmytro <pase4nikdv@gmail.com>
2023-03-29 10:20:20 +0200

    added poster to movieList

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -2,12 +2,17 @@ import styles from './MovieList.module.css';
 import { Link } from 'react-router-dom';
 import { getMovieDitailPage } from '../../routes';
 import PropTypes from 'prop-types';
+import defaultImage from '../../assets/default.jpg';
 
 const MovieList = ({ movies, query }) => {
   const location = query ? `/movies/?query=${query}` : '/';
+
   return (
     <ul className={styles.movieList}>
       {movies.map(movie => {
+        const posterPath = movie.poster_path
+          ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+          : defaultImage;
         return (
           <li key={movie.id} className={styles.movieItem}>
             <Link
@@ -15,6 +20,12 @@ const MovieList = ({ movies, query }) => {
               className={styles.movieLink}
               state={{ from: location }}
             >
+              <img
+                className={styles.poster}
+                src={posterPath}
+                alt={movie.original_title}
+                width="240px"
+              />
               {movie.name ? movie.name : movie.title}
             </Link>
           </li>
